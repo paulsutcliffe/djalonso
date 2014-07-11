e0672880d331cc79a23177019f38e07e2d6d114d
Paul Sutcliffe <paul@kosmyka.com>
2014-07-11 14:31:56 -0500

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Djalonso::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile = ['megafolio/js/jquery.themepunch.plugins.min.js', 'megafolio/js/jquery.themepunch.megafoliopro.js']
+  config.assets.precompile = ['megafolio/js/jquery.themepunch.plugins.min.js', 'megafolio/js/jquery.themepunch.megafoliopro.js', 'megafolio/fancybox/jquery.fancybox.pack.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
